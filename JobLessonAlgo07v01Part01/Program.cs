a853baf7b209ff8dbcc1bef882df19f4254b3372
Иван Сафронов <safroniv@mail.ru>
2022-07-18 00:02:05 +0300

    GB - Алгоритмы. Урок 7 Задание 1. готово.

--- JobLessonAlgo07v01Part01/Program.cs
+++ JobLessonAlgo07v01Part01/Program.cs
@@ -2,4 +2,13 @@
 // клетки в правую нижнюю. Известно что ходить можно только на одну клетку вправо или вниз.
 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Для прямоугольного поля размера M на N клеток");
+Console.WriteLine("Введите параметр M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Спасибо.");
+Console.WriteLine("Теперь введите параметр M:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Здорово. подождите немного....");
+int[,] tableMassive = new int[m - 1, n - 1];
+Thread.Sleep(5000);
+Console.WriteLine($"А вот и результат - количество путей = {WaysOfBoard.MathWaysOfBoard(m - 1, n - 1)}");
---
+++ JobLessonAlgo07v01Part01/WaysOfBoard.cs
@@ -0,0 +1,13 @@
+﻿// Для прямоугольного поля размера M на N клеток, подсчитать количество путей из верхней левой
+// клетки в правую нижнюю. Известно что ходить можно только на одну клетку вправо или вниз.
+
+
+public class WaysOfBoard
+{
+    public static int MathWaysOfBoard (int m, int n)
+    {
+        if ((m == 0) || (n == 0))
+        { return 1; }
+        return MathWaysOfBoard(m-1,n)+MathWaysOfBoard(m,n-1);
+    }
+}
